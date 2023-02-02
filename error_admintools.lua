ddf431473f56b38702e7357de784bb47194a79c1
maksytkadesighner <120841824+maksytkadesighner@users.noreply.github.com>
2023-02-03 01:59:23 +0300

    Update error_admintools.lua

--- error_admintools.lua
+++ error_admintools.lua
@@ -1,6 +1,9 @@
+local encoding = require 'encoding'
+encoding.default = 'CP1251'
+local function u8d(s) return encoding.UTF8:decode(s) end
 require 'lib.sampfuncs'
 function onSystemMessage(msg, type, script)
     if msg:find('AdminTools%.luac?:%d+:.+') and type == 3 then
-        sampShowDialog(252,'���-�� ����� �� ���','{FFFFFF}��������� ����������������� ������ � ������� {00BFFF}Admin Tools{FFFFFF}.\n���� ������ ������ ����������� ��������� �������� ������� ���� � �� ��������� {00BFFF}vk.com/maksytka_desighner{FFFFFF}\n\n'..msg,'�������',_,0)
+        sampShowDialog(252,u8d'Что-то пошло не так',u8d'{FFFFFF}Произошла непредусмотренная ошибка в скрипте {00BFFF}Admin Tools{FFFFFF}.\nЕсли данная ошибка повторяется отправьте скриншот данного окна в ВК создателя {00BFFF}vk.com/maksytka_desighner{FFFFFF}\n\n'..msg,u8d'Закрыть',_,0)
     end
 end
